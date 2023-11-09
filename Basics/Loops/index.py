12cbe1fa2e3ce5056a021ab386c237483d02f62f
asari <muhamadasari@smkwikrama.sch.id>
2023-11-10 02:42:29 +0700

    minor changes

---
+++ Basics/Loops/index.py
@@ -0,0 +1,14 @@
+# # break
+# text = ["foo", "foo2", "foo3"]
+# for letter in text:
+#     if letter == 'foo3':
+#         break
+#     print('Current Letter before break :', letter)
+
+# for i in range(100):
+#     if i % 2 != 0:
+#         print(i)
+
+for j in range(99, 0, -1):
+    if j % 2 == 0:
+        print(j)
